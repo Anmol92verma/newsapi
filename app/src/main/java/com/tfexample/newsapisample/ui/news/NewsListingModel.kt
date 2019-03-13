91328efbdee448edbbbad2024d37a27f070d6ff5
Anmol Verma <anmol.verma4@gmail.com>
2019-03-13 17:51:14 +0530

    caching with room implemented, pending LRU cache

--- app/src/main/java/com/tfexample/newsapisample/ui/news/NewsListingModel.kt
+++ app/src/main/java/com/tfexample/newsapisample/ui/news/NewsListingModel.kt
@@ -4,7 +4,6 @@ import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
 import android.arch.persistence.room.TypeConverter
 import android.arch.persistence.room.TypeConverters
-import android.support.annotation.NonNull
 import android.text.format.DateUtils
 import com.google.gson.Gson
 import com.tfexample.newsapisample.dataproviders.ARTICLES_TABLE
